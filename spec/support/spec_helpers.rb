f07ba080162ad60beef96f17fb402d333e0d57dd
Faisal N Jawdat <faisal@faisal.com>
2026-04-23 19:54:52 +0100

    spec_helpers: return config and ledger objects for explicit injection in tests

--- spec/support/spec_helpers.rb
+++ spec/support/spec_helpers.rb
@@ -16,11 +16,13 @@ module SpecHelpers
     Aardi.reset!
     base = YAML.safe_load_file(File.join(SAMPLES_DIR, 'minimal_config.yml'))
     config_data = base.merge(overrides.transform_keys(&:to_s))
+    config = Aardi.config
     Tempfile.create(['config', '.yml']) do |file|
       file.write(config_data.to_yaml)
       file.flush
-      Aardi.config.load(file.path)
+      config.load(file.path)
     end
+    config
   end
 
   # :reek:ControlParameter
@@ -30,17 +32,26 @@ module SpecHelpers
     custom_renderer = Aardi::CustomRenderer.new
     markup_opts = Aardi.config[:markup_options] || {}
     markdown_renderer = Redcarpet::Markdown.new(custom_renderer, markup_opts)
-
-    Aardi.ledger[:custom_renderer] = custom_renderer
-    Aardi.ledger[:markdown_renderer] = markdown_renderer
-    Aardi.ledger[:content_hashes] = Aardi::ContentHashes.new('/nonexistent_test_hashes')
-    Aardi.ledger[:html_files] = Set.new
+    ledger = Aardi::Ledger.new
+    ledger[:custom_renderer] = custom_renderer
+    ledger[:markdown_renderer] = markdown_renderer
+    ledger[:content_hashes] = Aardi::ContentHashes.new('/nonexistent_test_hashes')
+    ledger[:html_files] = Set.new
 
     html = template_html || File.read(File.join(SAMPLES_DIR, 'minimal_template.html'))
     Tempfile.create(['template', '.html']) do |file|
       file.write(html)
       file.flush
-      Aardi.ledger[:template] = Aardi::Template.new(file.path, ledger: Aardi.ledger)
+      ledger[:template] = Aardi::Template.new(file.path, ledger:)
     end
+
+    # still update global for specs that haven't been migrated to explicit injection
+    Aardi.ledger[:custom_renderer] = ledger[:custom_renderer]
+    Aardi.ledger[:markdown_renderer] = ledger[:markdown_renderer]
+    Aardi.ledger[:content_hashes] = ledger[:content_hashes]
+    Aardi.ledger[:html_files] = ledger[:html_files]
+    Aardi.ledger[:template] = ledger[:template]
+
+    ledger
   end
 end
