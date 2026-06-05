00558384d45f7a4e3470ab96b6a54bf227f7483b
Faisal N Jawdat <faisal@faisal.com>
2026-06-06 00:43:25 +0100

    Metadata: memoize tags list

--- lib/aardi/metadata.rb
+++ lib/aardi/metadata.rb
@@ -4,17 +4,19 @@ module Aardi
   class Metadata
     KNOWN_KEYS = %w[Title Description Creation Updated Tags].freeze
 
+    attr_reader :tags
+
     def initialize(yaml_str = '', source = nil)
       yaml = YAML.safe_load(yaml_str, permitted_classes: [Time])
       @yaml = {}
       @yaml = yaml if yaml.is_a? Hash
+      @tags = @yaml['Tags']&.split&.sort
       confirm_keys(source)
     end
 
     def creation    = @yaml['Creation']
     def description = @yaml['Description']
     def empty?      = @yaml.empty?
-    def tags        = @yaml['Tags']&.split&.sort
     def title       = @yaml['Title']
     def updated     = @yaml['Updated']
 
